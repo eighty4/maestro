1ab2d12a651bd36337f6d7f3976122bf5ce8922e
Adam McKee <adam.be.g84d@gmail.com>
2023-01-22 20:26:41 -0600

    replacing webpack and react with vite and svelte

--- frontend/src/Maestro.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-
-const Maestro = () => {
-    return <div>Maestro</div>
-}
-
-ReactDOM.render(<Maestro/>, document.getElementById('app'))
