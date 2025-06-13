94e5d9371ba83b39d084d2479b497d928a83ce85
Adam McKee <adam.be.g84d@gmail.com>
2025-06-13 11:09:51 -0500

    adding git hook support to ci verify script

--- ci_verify.sh
+++ ci_verify.sh
@@ -1,6 +1,26 @@
 #!/bin/sh
 set -e
 
+install_git_hook() {
+    local _hook=".git/hooks/$1"
+    if [ -e "$_hook" ]; then
+      echo "$_hook already exists"
+      exit 1
+    fi
+    ln -s $(realpath ci_verify.sh) $_hook
+    echo "linked ci_verify.sh to $_hook"
+    exit 0
+}
+
+for arg in "$@"; do
+  case $arg in
+    "--on-git-commit")
+      install_git_hook pre-commit;;
+    "--on-git-push")
+      install_git_hook pre-push;;
+  esac
+done
+
 # run through all the checks done for ci
 
 _git_status_output=$(git status --porcelain)
